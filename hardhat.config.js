3f1b9135895f81504a1222c4777499e822bdbd75
Gospel Darlington <darlingtongospel@gmail.com>
2024-06-10 14:57:22 +0100

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,11 +8,6 @@ module.exports = {
     localhost: {
       url: 'http://127.0.0.1:8545',
     },
-    sepolia: {
-      url: 'https://eth-sepolia.g.alchemy.com/v2/nUTdVHx8zxFQvxD8XbuTtSzeavPL6Pga',
-      accounts: ['0x84b20ebf8ef5f0a871f088df888e12bc8905fe5cc81702f0da9c509ee0119b2d'],
-      chainId: 11155111,
-    },
   },
   solidity: {
     version: '0.8.17',
